6481b0d117c1839afc3f60490126bba15c2d64dc
sue herrera <sueherrera30@gmail.com>
2017-09-05 12:36:10 -0500

    consumiendo informacion

--- js/blinkUtils.js
+++ js/blinkUtils.js
@@ -1,1 +1,38 @@
-var url = "git remote add origin https://github.com/sueherrera30/zeplin.git";
+var url ="http://demo6292426.mockable.io/blickTest";
+
+
+var btnContacto = document.getElementById("contacto-btn");
+btnContacto.addEventListener("click", getInfo);
+
+
+function getInfo(){
+	
+var peticion = new XMLHttpRequest();
+peticion.open('GET', url);
+peticion.onload = function(){
+	var data = JSON.parse(peticion.responseText);
+	var nombre = data.name;
+	console.log(nombre);
+	
+	var email = data.email;
+	console.log(email);
+    
+	alert(nombre+=email)
+	alert(infoAmigos(data));
+
+}
+peticion.send();
+}
+
+
+function infoAmigos(data){
+		
+	var amigos = data.friends;
+	console.log(amigos);
+	amigos.forEach(function(amigo){
+		var  nombreAmigo = amigo.name;
+		var edadAmigo = amigo.age;
+		var idAmigo = amigo.id;
+		alert(nombreAmigo+=edadAmigo+=idAmigo);
+	})      
+	};
